9c14061a07db08f411177dfdb49312bae549ee30
Eric Schrag <eschrag@gmail.com>
2015-12-05 20:49:44 -0500

    Use Anko for basic layout and dummy view to start

--- app/src/main/java/net/ericschrag/takotlin/MainActivity.kt
+++ app/src/main/java/net/ericschrag/takotlin/MainActivity.kt
@@ -2,11 +2,27 @@ package net.ericschrag.takotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.TextView
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
 // https://github.com/evz/tacofancy-api
+    var tacoRecipe : TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContentView(verticalLayout {
+            tacoRecipe = textView()
+            button {
+                textResource = R.string.refresh_label
+                onClick { tacoRecipe!!.text = "Cook ground beef for " + System.currentTimeMillis() + "ms and place in taco shell" }
+            }.lparams {
+                width = dip(88)
+                gravity = Gravity.CENTER_HORIZONTAL
+            }
+        })
     }
+
 }
