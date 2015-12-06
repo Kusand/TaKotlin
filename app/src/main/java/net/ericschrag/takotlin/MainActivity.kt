bcb58b1aa46eed4d1e317e4a37a517fa92c0922b
Eric Schrag <eschrag@gmail.com>
2015-12-05 19:27:01 -0500

    Initial Hello World

---
+++ app/src/main/java/net/ericschrag/takotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.ericschrag.takotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+// https://github.com/evz/tacofancy-api
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
